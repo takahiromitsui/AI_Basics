bd74049ff5ea4da9edd8a77ac49f1a651c9345e1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-04-23 21:58:09 +0200

    feat: Implement all except for minimax

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -40,10 +41,10 @@ def actions(board):
     Returns set of all possible actions (i, j) available on the board.
     """
     actions = set()
-    for row in board:
-        for cell in row:
+    for i, row in enumerate(board):
+        for j, cell in enumerate(row):
             if cell == EMPTY:
-                actions.add((board.index(row), row.index(cell)))
+                actions.add((i, j))
     return actions
 
     # raise NotImplementedError
@@ -53,6 +54,12 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    if action not in actions(board):
+        raise ValueError
+    else:
+        new_board = copy.deepcopy(board)
+        new_board[action[0]][action[1]] = player(board)
+        return new_board
     # raise NotImplementedError
 
 
@@ -60,6 +67,17 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] != EMPTY:
+            return row[0]
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != EMPTY:
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != EMPTY:
+        return board[0][2]
+    return None
     # raise NotImplementedError
 
 
@@ -67,6 +85,12 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    if winner(board) is not None:
+        return True
+    for row in board:
+        for cell in row:
+            if cell == EMPTY:
+                return False
     # raise NotImplementedError
 
 
@@ -74,6 +98,12 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
     # raise NotImplementedError
 
 
@@ -81,4 +111,9 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    # raise NotImplementedError
+    # if terminal(board):
+    #     return None
+
+    return (1,1)
+
+
